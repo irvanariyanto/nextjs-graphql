044abb7a8c660db9d75217b3539c8a089a75d10d
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2023-05-01 17:11:47 +0700

    hello world graphql on page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
+import useSWR from 'swr'
+
+const fetcher = (query: string) =>
+  fetch('api/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  })
+    .then((res) => res.json())
+    .then((json) => json.data)
+
+type Data = {
+  hello: string
+}
+
 export default function Home() {
+  const { data, error, isLoading } = useSWR<Data>('{hello}', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+  if (!data) return null
+
+  const { hello } = data
+
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center">Welcome</h1>
+      <h1 className="text-4xl font-bold text-center">{hello} </h1>
     </div>
   )
 }
